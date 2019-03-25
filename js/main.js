56a0b3f112428a48af145d563b3e30e7c0cf3ac4
myregister <boruyave@digitalmail.info>
2019-03-25 16:17:07 -0100

    int commit

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$('document').ready(function(){
+	$('#yslygi').click(function(){
+			window.scroll({
+	  		top: 750, 
+	  		left: 0, 
+	  		behavior: 'smooth'
+		});
+	});
+	$('#o-nas').click(function(){
+			window.scroll({
+	  		top: 1540, 
+	  		left: 0, 
+	  		behavior: 'smooth'
+		});
+	});
+	$('#contacts').click(function(){
+			window.scroll({
+	  		top: 2800, 
+	  		left: 0, 
+	  		behavior: 'smooth'
+		});
+	});
+	$('.btn-success').click(function(){
+		alert('Функция ещё не работает. Повторите попытку позже')
+	});
+});
